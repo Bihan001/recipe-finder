ca30b3bda4e844da5de9961e99fd073959616583
Bihan Chakraborty <bhchakraborty001@gmail.com>
2020-07-17 22:48:43 +0530

    heroku first commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const config = require('config');
+const path = require('path');
 const mongooseConnect = require('./Database/MongooseConnect');
 
 mongooseConnect();
@@ -11,5 +12,14 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/', require('./Routes/api/routes'));
+
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
